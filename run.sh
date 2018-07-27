8f47e21cbdcf73f7c81c561d5ce98cf27416e383
Erich Cordoba <erich.cordoba.malibran@intel.com>
2018-07-27 11:51:28 -0500

    First test

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clone the stx-tools repository.
+
+if ! git clone https://git.openstack.org/openstack/stx-tools; then
+    echo "Cannot clone the repository"
+    exit 1
+fi
+
+# Create docker image for mirror check
+
+pushd stx-tools/centos-mirror-tools
+# Temporal hack
+git fetch https://git.openstack.org/openstack/stx-tools refs/changes/58/585858/3 && git checkout FETCH_HEAD
+if ! docker build -t stx-mirror -f Dockerfile .; then
+    echo "Cannot create docker image"
+    exit 1
+fi
+
+# Run the image
+
+docker run -v $(pwd):/localdisk stx-mirror ./mirror-check.sh
